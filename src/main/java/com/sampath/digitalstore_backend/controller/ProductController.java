27ac141a508edfdf7f83728c3bb8b902a0d58d27
Sampath Menuka Chandimal <sampath.m.chandimal99@gmail.com>
2026-02-02 15:21:37 +0530

    add product and user controllers, services, and repositories

---
+++ src/main/java/com/sampath/digitalstore_backend/controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.sampath.digitalstore_backend.controller;
+
+public class ProductController {
+    // Controller logic for products
+}
---
+++ src/main/java/com/sampath/digitalstore_backend/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.sampath.digitalstore_backend.repository;
+
+public interface ProductRepository {
+    // Repository logic for products
+}
