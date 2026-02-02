6009d56f490854c465ddee453a96685623481da1
Sampath Menuka Chandimal <sampath.m.chandimal99@gmail.com>
2026-02-02 15:10:10 +0530

    initial commit

---
+++ src/main/java/com/sampath/digitalstore_backend/DigitalstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sampath.digitalstore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampath/digitalstore_backend/DigitalstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampath.digitalstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
