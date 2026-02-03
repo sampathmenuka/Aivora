0ac5dd45519ae0f64b272429e9cbf397d6dba264
Sampath Menuka Chandimal <sampath.m.chandimal99@gmail.com>
2026-02-03 20:04:21 +0530

    add OrderItem entity and role enumeration; update OrderResponse, ProductRequest, and ProductResponse DTOs with Lombok annotations

--- src/main/java/com/sampath/digitalstore_backend/dto/product/ProductRequest.java
+++ src/main/java/com/sampath/digitalstore_backend/dto/product/ProductRequest.java
@@ -1,5 +1,18 @@
 package com.sampath.digitalstore_backend.dto.product;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductRequest {
-    // Fields and methods for product request DTO
+
+    private String title;
+    private String description;
+    private Double price;
+    private String productType;
+    private String fileUrl;
+    private String previewUrl;
 }
