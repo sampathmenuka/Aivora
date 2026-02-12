22cd158a509fee81e2de0bd887a8b23df4d14f23
Sampath Menuka Chandimal <sampath.m.chandimal99@gmail.com>
2026-02-12 14:56:26 +0530

    feat: add Cart and CartItem repositories for shopping cart management

---
+++ src/main/java/com/sampath/digitalstore_backend/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.sampath.digitalstore_backend.repository;
+
+import com.sampath.digitalstore_backend.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+}
